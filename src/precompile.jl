2c4e6860540e68f9eb43d470c64ffc71df94af4c
cgarling <chris.t.garling@gmail.com>
2025-10-19 18:49:26 -0400

    Run CI on 1.10 minimum Julia version

--- src/precompile.jl
+++ src/precompile.jl
@@ -2,7 +2,7 @@ using PrecompileTools: @setup_workload, @compile_workload
 
 @setup_workload begin
     @compile_workload begin
-        for i in (Chabrier2003,Chabrier2001BPL,Chabrier2001LogNormal,Kroupa2001,Salpeter1955)
+        for i in (Chabrier2003, Chabrier2001BPL, Chabrier2001LogNormal, Kroupa2001, Salpeter1955)
             for T in (Float32, Float64)
                 I = i(T(0.08), T(100.0))
                 for f in (rand, mean, median)
