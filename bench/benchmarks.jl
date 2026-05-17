ba9661ccb7da0db78fe0d4c392ccc167d1d93388
cgarling <chris.t.garling@gmail.com>
2026-05-17 16:48:33 -0400

    start benchmark file

---
+++ bench/benchmarks.jl
@@ -0,0 +1,24 @@
+using BenchmarkTools
+using PrettyTables
+using InitialMassFunctions
+
+function show_benchmarks(results)
+    # Collect results
+    sorted  = sort(collect(results), by=first)
+    names   = [k for (k,_) in sorted]
+    trials  = [v for (_,v) in sorted]
+
+    # Pack into matrix
+    data = hcat(
+        names,
+        [BenchmarkTools.prettytime(median(t).time) for t in trials],
+        [BenchmarkTools.prettymemory(median(t).memory) for t in trials],
+        [median(t).allocs for t in trials]
+    )
+
+    # Make pretty table
+    pretty_table(data;
+        column_labels = ["Benchmark", "Median Time", "Memory", "Allocs"],
+        alignment     = [:l, :r, :r, :r]
+    )
+end
