dcf2c492ae2a360d5815672880295b815d0b628e
cgarling <chris.t.garling@gmail.com>
2026-05-17 21:26:00 -0400

    revise allocation tests
    I think they were failing because LLVM.jl was emitting deprecation warnings which triggered @test_nowarn

--- test/allocations_tests.jl
+++ test/allocations_tests.jl
@@ -12,12 +12,12 @@ for T in (Float32, Float64)
             if i ∈ (Chabrier2001LogNormal, Kroupa2001) # Constructors for which the allocations test do not pass
                 d = i(T(0.08), T(100.0))
             else
-                d = @test_nowarn test_imf_allocs_constructor(T, i)
+                d = test_imf_allocs_constructor(T, i)
             end
 
             for f in (rand, mean, median)
                 @testset "$(f)" begin
-                    @test_nowarn test_imf_allocs(d, f)
+                    test_imf_allocs(d, f)
                 end
             end
             # for f in (pdf, logpdf, cdf, ccdf, quantile, cquantile)
